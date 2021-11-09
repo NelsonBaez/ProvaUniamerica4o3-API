12da12c3ff9c7b4cd49f6c2fa630a8bda6e95dc0
Nelson J S Baez <bluexbaez@gmail.com>
2021-11-09 20:51:28 -0300

    Criado testes do CreateContact

--- src/test/java/com/example/prova/service/ContactServiceTest.java
+++ src/test/java/com/example/prova/service/ContactServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.prova.service;
 
 import com.example.prova.dto.ContactDTO;
+import com.example.prova.dto.input.NewContactDTO;
+import com.example.prova.exceptions.UniqueException;
 import com.example.prova.model.Contact;
 import com.example.prova.repository.ContactRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,9 +13,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class ContactServiceTest {
@@ -29,12 +34,8 @@ class ContactServiceTest {
 
     @Test
     void shouldReturnAllContacts() {
-        Contact contact = Contact.builder()
-                .id(1L)
-                .name("jorge")
-                .email("jorge@gmail.com")
-                .phone("88888888")
-                .build();
+        var contact = correctContact();
+
         Mockito.when(contactRepository.findAll()).thenReturn(List.of(contact));
 
         var result = contactService.findAll();
@@ -42,6 +43,53 @@ class ContactServiceTest {
         List<ContactDTO> contacts = new ArrayList<>();
         contacts.add(new ContactDTO(contact));
         assertThat(result).isEqualTo(contacts);
+    }
+
+    @Test
+    void shouldCreateContact(){
+        var newContactDTO = correctNewContactDTO();
+        var contact = correctContact();
+
+        Mockito.doReturn(contact).when(contactRepository).save(any(Contact.class));
+
+        var result = contactService.create(newContactDTO);
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(new ContactDTO(contact));
+    }
+
+    @Test
+    void shouldReturnUniqueExceptionIfEmailNotUnique() {
+        var contact = correctContact();
+
+        Mockito.when(contactRepository.findByEmail(any())).thenReturn(Optional.of(contact));
+
+        assertThatThrownBy(() -> contactService.ifEmailExistsReturnException("jorge")).isInstanceOf(UniqueException.class);
+    }
+
+    @Test
+    void shouldReturnUniqueExceptionIfPhoneNotUnique() {
+        var contact = correctContact();
+
+        Mockito.when(contactRepository.findByPhone(any())).thenReturn(Optional.of(contact));
+
+        assertThatThrownBy(() -> contactService.ifPhoneExistsReturnException("888888888")).isInstanceOf(UniqueException.class);
+    }
+
+    Contact correctContact(){
+        return Contact.builder()
+               .id(1L)
+               .name("jorge")
+               .email("jorge@gmail.com")
+               .phone("88888888")
+               .build();
+    }
+    NewContactDTO correctNewContactDTO(){
+        return NewContactDTO.builder()
+                .name("jorge")
+                .email("jorge@gmail.com")
+                .phone("88888888")
+                .build();
     }
 
 }
