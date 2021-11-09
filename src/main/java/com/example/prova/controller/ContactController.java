5d2320cceffa9a406301ebbc23f189c01435191a
Nelson J S Baez <bluexbaez@gmail.com>
2021-11-09 19:40:17 -0300

    Implementado Funcionalidade Index dos contatos

--- src/main/java/com/example/prova/controller/ContactController.java
+++ src/main/java/com/example/prova/controller/ContactController.java
@@ -1,11 +1,26 @@
 package com.example.prova.controller;
 
 
+import com.example.prova.model.Contact;
+import com.example.prova.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/users")
+import java.util.List;
+
+@RequestMapping("/contacts")
 @RequiredArgsConstructor
+@RestController
 public class ContactController {
+    private final ContactService contactService;
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Contact> index(){
+        return contactService.findAll();
+    }
 }
--- src/main/java/com/example/prova/service/ContactService.java
+++ src/main/java/com/example/prova/service/ContactService.java
@@ -1,2 +1,20 @@
-package com.example.prova.service;public class ContactService {
+package com.example.prova.service;
+
+import com.example.prova.model.Contact;
+import com.example.prova.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepository contactRepository;
+
+    public List<Contact> findAll() {
+        return contactRepository.findAll();
+    }
 }
