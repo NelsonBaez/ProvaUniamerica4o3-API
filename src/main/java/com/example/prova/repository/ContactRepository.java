9a8161fab551dcbfba373c60c869219e65d0d78b
Nelson J S Baez <bluexbaez@gmail.com>
2021-11-09 20:20:59 -0300

    Adicionado validação se email ou phone ja existe

--- src/main/java/com/example/prova/repository/ContactRepository.java
+++ src/main/java/com/example/prova/repository/ContactRepository.java
@@ -3,5 +3,10 @@ package com.example.prova.repository;
 import com.example.prova.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findByEmail(String username);
+
+    Optional<Contact> findByPhone(String phone);
 }
--- src/main/java/com/example/prova/service/ContactService.java
+++ src/main/java/com/example/prova/service/ContactService.java
@@ -1,12 +1,12 @@
 package com.example.prova.service;
 
+import com.example.prova.exceptions.UniqueException;
 import com.example.prova.model.Contact;
 import com.example.prova.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Service
@@ -20,6 +20,22 @@ public class ContactService {
     }
 
     public Contact create(Contact contact) {
+        ifEmailExistsReturnException(contact.getEmail());
+        ifPhoneExistsReturnException(contact.getPhone());
         return contactRepository.save(contact);
+    }
+
+    public void ifEmailExistsReturnException(String email){
+        var contact = contactRepository.findByEmail(email);
+        if(contact.isPresent()){
+            throw new UniqueException("Email já está cadastrado");
+        }
+    }
+
+    public void ifPhoneExistsReturnException(String phone){
+        var contact = contactRepository.findByPhone(phone);
+        if(contact.isPresent()){
+            throw new UniqueException("Telefone já está cadastrado");
+        }
     }
 }
